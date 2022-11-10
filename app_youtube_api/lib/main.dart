467750d3159a9024744e382f0fd54057999cc168
Giovanna Coelho <giovanna2683@gmail.com>
2022-11-09 21:35:01 -0400

    Implementação do método de busca e listagem dos resultados

--- app_youtube_api/lib/main.dart
+++ app_youtube_api/lib/main.dart
@@ -28,9 +28,11 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  bool typing = false;
+  String header = "Pelo que você está procurando?";
   static String api_key = "AIzaSyDiiwePwEEhi6E3Rv77iar2gUgCs0EybL0";
 
-  List<Object> results = [];
+  List<YouTubeVideo> results = [];
 
   YoutubeAPI yt = YoutubeAPI(api_key);
 
@@ -42,9 +44,12 @@ class HomePageState extends State<HomePage> {
 
   callApi() async {
     try {
-      results = await yt.search("jorge");
-      setState(() {
-      });
+      results = await yt.search(
+        TextBox.ytsearch.text,
+        order: 'relevance',
+        videoDuration: 'any',
+      );
+      setState(() {});
     } catch (e) {
       print(e);
     }
@@ -53,21 +58,98 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[850],
       appBar: AppBar(
-        title: Text(widget.title),
+        toolbarHeight: 80,
+        backgroundColor: Colors.grey[900],
+        title: typing ? TextBox() : Text(header),
+        leading: IconButton(
+          icon: Icon(typing ? Icons.done : Icons.search),
+          onPressed: () {
+            setState(() {
+              typing = !typing;
+            });
+            if (typing == false) {
+              callApi();
+            }
+            if (TextBox.ytsearch.text == null) {
+              header = 'Pelo que você está procurando?';
+            } else {
+              header = TextBox.ytsearch.text;
+            }
+          },
+        ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Projeto Youtube API - LIA',
-            ),
-            Text(
-              'LIA',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: ListView(
+        children: results.map<Widget>(listItem).toList(),
+      ),
+    );
+  }
+
+  Widget listItem(YouTubeVideo video) {
+    return GestureDetector(
+      child: Card(
+        color: Colors.grey[850],
+        child: Container(
+          margin: EdgeInsets.symmetric(vertical: 7.0),
+          padding: EdgeInsets.all(12.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(right: 20.0),
+                child: Image.network(
+                  video.thumbnail.small.url ?? '',
+                  width: 120.0,
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      video.title,
+                      softWrap: true,
+                      style: TextStyle(fontSize: 18.0, color: Colors.grey[500]),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 3.0),
+                      child: Text(
+                        video.description ?? '',
+                        maxLines: 3,
+                        softWrap: true,
+                        style: TextStyle(
+                            fontWeight: FontWeight.normal, color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TextBox extends StatelessWidget {
+  static TextEditingController ytsearch = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      color: Colors.grey[850],
+      child: Padding(
+        padding: const EdgeInsets.only(right: 50),
+        child: TextField(
+          decoration:
+              InputDecoration(border: InputBorder.none, hintText: 'Busque'),
+          controller: ytsearch,
+          style: TextStyle(color: Colors.white)
         ),
       ),
     );
